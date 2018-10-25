50e26c179b7ea3b1e1378c78debd89f8c1ee463d
Luis Reyes <larcman@gmail.com>
2018-10-25 15:06:28 -0500

    Created Users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_153905) do
+ActiveRecord::Schema.define(version: 2018_10_25_195557) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+  
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
